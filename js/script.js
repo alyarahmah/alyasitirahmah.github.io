2486140ac8a75cfa0dd512fc4d1cbf90a0bd4594
alyarahmah <alyarahmah1205@gmail.com>
2025-01-28 21:21:57 +0700

    push semua

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// ====================== Scroll Section Activated Link =======================//
+let Section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    Section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset +height) {
+            navLinks.forEach(links => {
+                links.
+            })
+        }
+    })
+}
